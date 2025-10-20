63d9d8ed52bc40ecda135a97dbb26aa0bb7f3ea0
Eldreney <Ahmed.Yehia@raqmia.net>
2025-10-21 01:58:46 +0300

    added initial services

--- obj/Debug/net9.0/PlatformService.AssemblyInfo.cs
+++ obj/Debug/net9.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc126c115a5394a2a5824fe68cb4e464729c03c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
