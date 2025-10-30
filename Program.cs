05716826d9c7b8b22db31d85e7dcc6d05c276b9c
Eldreney <Ahmed.Yehia@raqmia.net>
2025-10-31 01:12:41 +0200

    registered dbcontext

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-   builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt=>opt.UseInMemoryDatabase("InMem"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
